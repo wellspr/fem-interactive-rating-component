6caba9f17c1bd0a012a0435ca8d5ad1c9fddc0c4
Paulo <paulowells@gmail.com>
2022-04-15 08:29:34 -0300

    added accessibility suport

--- public/js/index.js
+++ public/js/index.js
@@ -22,29 +22,44 @@ const ratingBullets = document.querySelectorAll(".rating-bullet");
 Object.values(ratingBullets).map((bullet, index) => {
     const selectRating = () => {
         bullet.classList.add("selected");
+        bullet.ariaSelected = "true";
         ratingValue = bullet.textContent;
         document.getElementById("rating-value").innerHTML = ratingValue;
+        document.querySelector(".rating-feedback").setAttribute("aria-label", `You selected ${ratingValue} out of 5`)
         const selectedIndex = index;
         
         Object.values(ratingBullets).map((bullet, index) => {
             if (index !== selectedIndex) {
                 bullet.classList.remove("selected");
+                bullet.ariaSelected = "false";
             }
         });
     };
 
     bullet.addEventListener("click", selectRating);
+    bullet.addEventListener("keydown", e => {
+        if (e.keyCode === 32) {
+            selectRating();
+        }
+    });
 
     return null;
 });
 
+const form = document.querySelector("form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+});
+form.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        submitBtn.click();
+    }
+})
+
 // Submit
-const submitBtn = document.getElementById("submit");
+const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", () => {
-    if (ratingValue === "") {
-        alert("Please select a rating.")
-        return null;
-    };
+    if (ratingValue === "") return null;
     setActivePage("thank-you");
 });
